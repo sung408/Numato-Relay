59d214b63ff1a825858c776431290aac265c2a91
Sung W Chung <sung408@gmail.com>
2023-09-15 10:50:17 -0600

    Update Numato 16ch relay.py
    channel =>10 are A, B, C,....

--- Numato 16ch relay.py
+++ Numato 16ch relay.py
@@ -1,83 +1,57 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[73]:
-
-
 import sys
 import serial
+import time
+
+channel_conversion={10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
 
 #Relay  Find serial port number (eg. COOM5) in Device Manager for Windows
 numato1= serial.Serial("COM5", 9600)
-
-if not numato1.isOpen():
+if not numato1.isOpen():    
     print ('Numator board not communicating....')
-
-
-# In[74]:
-
-
-def relay_off(board, channel):
-    cmd='relay off ' + str(channel)+ '\n\r'
-    command=bytes(cmd.encode())
-    board.write(command)    
-    return cmd
-
-
-# In[75]:
-
+else:
+    print ('connected')
 
 def relay_on(board, channel):
-    cmd='relay on ' + str(channel)+ '\n\r'
+    if channel<10:
+        ch=str(channel)
+    else:
+        ch=channel_conversion[channel]    
+    
+    cmd='relay on ' + ch + '\n\r'
+    command=bytes(cmd.encode())
+    board.write(command)    
+    return cmd
+
+def relay_off(board, channel):
+    if channel<10:
+        ch=str(channel)
+    else:
+        ch=channel_conversion[channel]     
+    cmd='relay off ' + ch + '\n\r'
     command=bytes(cmd.encode())
     board.write(command)    
     return cmd
 
 
-# In[79]:
-
-
-relay_on(numato1,1)
-
-
-# In[80]:
-
-
-relay_off(numato1,1)
-
-
-# In[81]:
-
-
-relay_on(numato1,1)
-
-
-# In[82]:
-
-
-relay_off(numato1,1)
-
-
-# In[50]:
+def all_channel_off(board, total_channel=16):
+        for i in range(total_channel):            
+            relay_off(board, i)
+            time.sleep(0.2)
 
 
 
+def all_channel_off(board, total_channel=16):
+        for i in range(total_channel):            
+            relay_off(board, i)
+            time.sleep(0.2)
 
 
-# In[68]:
 
+relay_on(numato1,11)
+relay_off(numato1,11)
 
-
-
-
-# In[72]:
-
-
-numato1.close()
-
-
-# In[ ]:
-
+all_channel_on(numato1)
+all_channel_off(numato1)
 
 
 
